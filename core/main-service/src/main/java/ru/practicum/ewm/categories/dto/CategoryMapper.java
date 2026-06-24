c3aed3cc23a9e39706022c76c492c613bffac4ac
Denis <dessitnov22@yandex.ru>
2026-06-24 11:44:11 +0300

    spring-cloud

---
+++ core/main-service/src/main/java/ru/practicum/ewm/categories/dto/CategoryMapper.java
@@ -0,0 +1,33 @@
+package ru.practicum.ewm.categories.dto;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.ewm.categories.model.Category;
+
+@Component
+public class CategoryMapper {
+    public static Category toCategory(CategoryDto categoryDto) {
+        return Category.builder()
+                .id(categoryDto.getId())
+                .name(categoryDto.getName())
+                .build();
+    }
+
+    public static Category toCategory(NewCategoryDto newCategoryDto) {
+        return Category.builder()
+                .name(newCategoryDto.getName())
+                .build();
+    }
+
+    public static CategoryDto toCategoryDto(Category category) {
+        return CategoryDto.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
+    }
+
+    public static NewCategoryDto toNewCategoryDto(Category category) {
+        return NewCategoryDto.builder()
+                .name(category.getName())
+                .build();
+    }
+}
