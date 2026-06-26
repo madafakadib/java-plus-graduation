b4de59db74790d815db8ef95242173d3bf488c52
Denis <dessitnov22@yandex.ru>
2026-06-26 22:22:25 +0300

    spring-cloud

--- core/main-service/src/main/java/ru/practicum/ewm/EwmService.java
+++ core/main-service/src/main/java/ru/practicum/ewm/EwmService.java
@@ -2,11 +2,15 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import ru.practicum.stat.client.StatsClient;
 
 @SpringBootApplication
 @Import(StatsClient.class)
+@EntityScan("ru.practicum.ewm")
+@EnableJpaRepositories("ru.practicum.ewm")
 public class EwmService {
     public static void main(String[] args) {
         SpringApplication.run(EwmService.class, args);
